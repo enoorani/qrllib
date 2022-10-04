bc37cd41c0af3a5cea7ec88d9c2bdfb13696d675
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 19:46:58 +0000

    Adding numeric_owner as keyword arguement

--- release/tune_tests/cloud_tests/workloads/run_cloud_test.py
+++ release/tune_tests/cloud_tests/workloads/run_cloud_test.py
@@ -421,7 +421,7 @@ def fetch_remote_directory_content(
                     if not is_within_directory(path, member_path):
                         raise Exception("Attempted Path Traversal in Tar File")
             
-                tar.extractall(path, members, numeric_owner) 
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
                 
             
             safe_extract(tar, dir)
